c0033ca4fb60e7dcdc4887660c1ef57294888362
VISHAL SHAKYA <shakayavishal007@gmail.com>
2022-06-11 12:16:29 +0530

    add profile edit icon

--- lib/Components/StartupView/StartupInfoSection/Picture.dart
+++ lib/Components/StartupView/StartupInfoSection/Picture.dart
@@ -1,33 +1,54 @@
 import 'package:be_startup/Utils/Colors.dart';
 import 'package:be_startup/Utils/Messages.dart';
+import 'package:be_startup/Utils/Routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class Picture extends StatelessWidget {
   String? logo = temp_logo;
 
-  Picture({
-    this.logo, 
-    Key? key}) : super(key: key);
+  Picture({this.logo, Key? key}) : super(key: key);
+
+  EditBusinessLogo() {
+    Get.toNamed(create_business_detail_url);
+  }
 
   @override
   Widget build(BuildContext context) {
-  double profile_top_pos = 0.19;
-  double profile_left_pos = 0.01;
+    double profile_top_pos = 0.19;
+    double profile_left_pos = 0.01;
     return Positioned(
-      top: context.height * profile_top_pos,
-      left: context.width * profile_left_pos,
-      child: Card(
-        elevation: 5,
-        shadowColor: light_color_type3,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(62)),
-        child: Container(
-            child: CircleAvatar(
-          radius: 60,
-          backgroundColor: Colors.blueGrey[100],
-          foregroundImage: NetworkImage(logo!)
-        ),
-      ),
-    ));
+        top: context.height * profile_top_pos,
+        left: context.width * profile_left_pos,
+        child: Stack(
+          children: [
+            Card(
+              elevation: 5,
+              shadowColor: light_color_type3,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(62)),
+              child: Container(
+                child: CircleAvatar(
+                    radius: 60,
+                    backgroundColor: Colors.blueGrey[100],
+                    foregroundImage: NetworkImage(logo!)),
+              ),
+            ),
+            Positioned(
+              top: context.height * 0.09,
+              left: context.width * 0.05,
+              child: Container(
+                padding: EdgeInsets.all(5),
+                child: IconButton(
+                  onPressed: () {
+                    EditBusinessLogo();
+                  },
+                  icon: Icon(
+                    Icons.edit,
+                  size: 18,
+                  color:light_color_type3)),
+              ))
+          ],
+        ));
   }
 }
